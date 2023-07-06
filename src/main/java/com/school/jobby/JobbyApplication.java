8e8008674c9627f03569c10f1653319f08e30995
Larissa Andrade <larissa.sandrade@hotmail.com>
2023-07-06 09:14:46 -0300

    projeto criado

---
+++ src/main/java/com/school/jobby/JobbyApplication.java
@@ -0,0 +1,13 @@
+package com.school.jobby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobbyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobbyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/school/jobby/JobbyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.jobby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobbyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
