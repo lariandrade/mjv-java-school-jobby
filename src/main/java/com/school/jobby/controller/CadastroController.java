0e8a88b6c752c7d03dcf4ea95d56298d00b042c0
Larissa Andrade <larissa.sandrade@hotmail.com>
2023-07-10 15:00:35 -0300

    criado controller e repository

---
+++ src/main/java/com/school/jobby/controller/CadastroController.java
@@ -0,0 +1,10 @@
+package com.school.jobby.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/candidatos")
+public class CadastroController {
+
+}
---
+++ src/main/java/com/school/jobby/repository/CadastroRepository.java
@@ -0,0 +1,7 @@
+package com.school.jobby.repository;
+
+import com.school.jobby.models.Cadastro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CadastroRepository extends JpaRepository<Cadastro, Long> {
+}
