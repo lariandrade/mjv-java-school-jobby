43e06db9dc5894770bcbc8aa1138565fb443d274
Larissa Andrade <larissa.sandrade@hotmail.com>
2023-07-16 13:37:50 -0300

    feat: adicionar configurações do Swagger

---
+++ src/main/java/com/school/jobby/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package com.school.jobby;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Jobby")
+                        .description("API Cadastro de Profissionais.")
+                        .contact(new Contact()
+                                .name("Larissa Andrade")
+                                .email("larissa.sandrade@hotmail.com"))
+                        );
+    }
+}
